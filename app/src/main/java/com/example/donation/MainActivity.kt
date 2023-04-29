180fbf29f0600ab7d8c5cc4bc1eae6dbdc1d64e2
leev0305 <leevese@gmail.com>
2023-04-29 14:20:40 +0800

    Re-added viewbinding

--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.donation
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.donation.databinding.ActivityMainBinding
@@ -7,6 +8,7 @@ import com.example.donation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
