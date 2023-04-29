bf0a861f353304ad1ad72012de20b66a9e789fdd
leev0305 <leevese@gmail.com>
2023-04-29 14:33:08 +0800

    Removed the viewbinding and added back again

--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.donation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.donation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding : ActivityMainBinding
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//    }
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
