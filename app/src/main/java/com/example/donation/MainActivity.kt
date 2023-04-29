e1480dd54255daeb777623c9deba2528abc72c62
Xiang1226 <limzx-wm21@student.tarc.edu.my>
2023-04-30 03:21:37 +0800

    done

--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.donation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.donation.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarItemView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -10,7 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //I am LZX
-        //I am LV
+
+        val bottomNavigationView = binding.bottomNavigationBar
+        val navController = findNavController(R.id.myNavHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
+
+//        NavigationBarView.OnItemSelectedListener {
+//            item -> when(item.itemId) {
+//                R.id.homeFragment -> { true }
+//                R.id.campaignFragment -> { true }
+//                R.id.profileFragment -> { true }
+//            else -> { false }
+//        }
+//        }
     }
 }
