f4a93e8b5d56e1879fbff217f084ab4a26ae9642
Xiang1226 <limzx-wm21@student.tarc.edu.my>
2023-04-30 21:16:19 +0800

    done

--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.donation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.fragment.app.Fragment
 import com.example.donation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.homeFragment -> replaceFragment(Home())
+                R.id.campaignFragment -> replaceFragment(Campaign())
+                R.id.profileFragment -> replaceFragment(Profile())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
