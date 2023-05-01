351430a908d078b49a60fe6f77be3be285f8c775
leev0305 <leevese@gmail.com>
2023-05-02 03:12:43 +0800

    Resolved conflict

--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.donation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.donation.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationBarView
@@ -17,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-<<<<<<< HEAD
         binding.triggerLogin.setOnClickListener {
             startLogin()
         }
@@ -27,19 +24,5 @@ class MainActivity : AppCompatActivity() {
         val loginIntent = Intent(this, LoginActivity::class.java)
         loginIntent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
         startActivity(loginIntent)
-=======
-        val bottomNavigationView = binding.bottomNavigationBar
-        val navController = findNavController(R.id.myNavHostFragment)
-        bottomNavigationView.setupWithNavController(navController)
-
-//        NavigationBarView.OnItemSelectedListener {
-//            item -> when(item.itemId) {
-//                R.id.homeFragment -> { true }
-//                R.id.campaignFragment -> { true }
-//                R.id.profileFragment -> { true }
-//            else -> { false }
-//        }
-//        }
->>>>>>> c000072fd42d2b17197be9345ee3b8f8cbc89462
     }
 }
