7016651b124fe8c7bfb315e1e99844b258fed65a
Xiang1226 <limzx-wm21@student.tarc.edu.my>
2023-05-02 03:21:42 +0800

    Basic Layout

--- app/src/main/java/com/example/donation/Campaign.kt
+++ app/src/main/java/com/example/donation/Campaign.kt
@@ -1,59 +1,67 @@
 package com.example.donation
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.findFragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.donation.databinding.FragmentCampaignBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Campaign.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Campaign : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class Campaign : Fragment(R.layout.fragment_campaign) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_campaign, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Campaign().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentCampaignBinding.bind(view)
+
+        val tabLayout = binding.tabLayout
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                // handle tab selection event
+                when (tab.position) {
+                    0 -> {
+                        replaceFragment(Donation())
+                    }
+                    1 -> {
+                        replaceFragment(Volunteer())
+                    }
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        })
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val childFragmentManager = childFragmentManager
+        childFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
 }
--- app/src/main/java/com/example/donation/MainActivity.kt
+++ app/src/main/java/com/example/donation/MainActivity.kt
@@ -1,35 +1,143 @@
 package com.example.donation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import com.example.donation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var currentFragment: Fragment
+
+    companion object {
+        private const val KEY_THEME_MODE = "theme_mode"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(Home())
 
-        binding.bottomNavigationBar.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.homeFragment -> replaceFragment(Home())
-                R.id.campaignFragment -> replaceFragment(Campaign())
-                R.id.profileFragment -> replaceFragment(Profile())
-                else -> {}
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val savedThemeMode =
+            prefs.getInt(KEY_THEME_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        // Set the theme mode according to the saved value
+        AppCompatDelegate.setDefaultNightMode(savedThemeMode)
+        setContentView(binding.root)
+        // Create the fragment
+        currentFragment = Home()
+        replaceFragment(currentFragment)
+
+        //no need to keep typing binding
+        binding.apply {
+            topBarMenu.setOnClickListener{drawerLayout.openDrawer(GravityCompat.START)}
+
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.notification -> {
+                        Toast.makeText(this@MainActivity, "first item selected", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    R.id.contact_us -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "second item selected",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                    R.id.about_us -> {
+                        Toast.makeText(this@MainActivity, "third item selected", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    R.id.setting -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "fourth item selected",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+                true
             }
-            true
+
+            val isDarkModeOn = savedThemeMode == AppCompatDelegate.MODE_NIGHT_YES
+            switchToggle.isChecked = isDarkModeOn
+            updateThemeTextView(isDarkModeOn)
+
+            switchToggle.setOnCheckedChangeListener { _, isChecked ->
+                Toast.makeText(applicationContext, "Lol", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                updateTheme(isChecked, prefs)
+            }
+
+            bottomNavigationBar.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.homeFragment -> replaceFragment(Home())
+                    R.id.campaignFragment -> replaceFragment(Campaign())
+                    R.id.profileFragment -> replaceFragment(Profile())
+                }
+                true
+            }
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateThemeTextView(isDarkModeOn: Boolean) {
+        if (isDarkModeOn) {
+            binding.currentTheme.text = "Night"
+        } else {
+            binding.currentTheme.text = "Day"
         }
     }
 
-    private fun replaceFragment(fragment : Fragment) {
+    private fun updateTheme(isDarkModeOn: Boolean, prefs: SharedPreferences) {
+        if (isDarkModeOn) {
+            prefs.edit().putInt(KEY_THEME_MODE, AppCompatDelegate.MODE_NIGHT_YES).apply()
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            prefs.edit().putInt(KEY_THEME_MODE, AppCompatDelegate.MODE_NIGHT_NO).apply()
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        recreate()
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        currentFragment = fragment
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Save the current fragment when the activity is paused or stopped
+        supportFragmentManager.putFragment(outState, "currentFragment", currentFragment)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Restore the current fragment when the activity is recreated
+        currentFragment =
+            supportFragmentManager.getFragment(savedInstanceState, "currentFragment") ?: Home()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, currentFragment)
+            .commit()
     }
 }
